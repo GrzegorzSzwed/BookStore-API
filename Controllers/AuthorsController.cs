c61f115d57140b5b1884eb395e6cd973a15265e8
GrzegorzSzwed <grzegorz.jan.szwed@gmail.com>
2020-10-04 15:49:21 +0200

    Build Author Endpoint, Repositories

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookStore_API.Contracts;
+using BookStore_API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// Endpoint used to iteract with the Authors in the book store's database
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+        public AuthorsController(IAuthorRepository authorRepository,
+            ILoggerService logger,
+            IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        /// <summary>
+        /// Get All Authors
+        /// </summary>
+        /// <returns>List of authors</returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAuthors()
+        {
+            try
+            {
+                _logger.LogInfo("Attempted Get All Authors.");
+                var authors = await _authorRepository.FindAll();
+                var response = _mapper.Map<IList<AuthorDTO>>(authors);
+                _logger.LogInfo("Successfully got all Authors.");
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"{e.Message} - {e.InnerException}");
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
